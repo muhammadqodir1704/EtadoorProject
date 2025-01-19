c83ffd79b666af15322913772460538e7b39faae
Sardorbek2414 <lutfiddinovsardor1@gmail.com>
2025-01-19 15:00:59 +0500

    kontakt qismi

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,10 @@
+import Contact from "./components/Contact";
+import "./App.css";
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">assalom</h1>;
+  return (
+    <h1 className="text-3xl font-bold">
+      <Contact />
+    </h1>
+  );
 }
