51579508cfe55342f577f9771c2041fd71446f73
Muhammadqodir <muhammadqodir@Macbook-Pro---valijonov.local>
2025-01-29 00:47:31 +0500

    Asosiy sahifa boldi , tablarni togrilab chiqish kerak

---
+++ src/components/KatalogDverey.jsx
@@ -0,0 +1,79 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Rasm from "./Rasm";
+import Xarita from "./Xarita";
+import Footer from "./Footer";
+import Input from "./Input";
+import Dverxiy from "./Dverxiy";
+
+const KatalogDverey = () => {
+  const [doors, setDoors] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+ const fetchDoors = async () => {
+        try {
+            const response = await fetch('https://etadoor.koyeb.app/api/doors?page=0&size=20', {
+                method: 'GET',
+                headers: {
+                    Accept: 'application/json',
+                },
+            });
+            const result = await response.json();
+            setProducts(result.data || []); // data massivini to'g'ridan-to'g'ri olamiz
+            setLoading(false);
+        } catch (error) {
+            console.error('API xatolik:', error);
+            setLoading(false);
+        }
+    };
+
+    fetchDoors();
+  }, []);
+
+  return (
+    <div>
+      <h1 className=" ms-6 mb-4">Каталог дверей</h1>
+      <div className="grid grid-cols-4 gap-5 justify-items-center">
+        {loading && <p>Loading doors...</p>}
+        {doors.map((item) => (
+          <div
+            key={item.id}
+            className="w-full max-w-xs p-4 bg-white rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300">
+            <img
+              src={item.images[0]}
+              alt={item.name}
+              className="w-full h-40 object-cover rounded-t-lg"
+            />
+            <div className="mt-3">
+              <h3 className="text-lg font-semibold text-gray-800">
+                {item.name}
+              </h3>
+              <p className="text-sm text-gray-600 mt-1">{item.description}</p>
+              <div className="mt-2">
+                <h3 className="text-gray-700">
+                  Price: <span className="font-medium">${item.price}</span>
+                </h3>
+                <h3 className="text-gray-700">
+                  Final Price:{" "}
+                  <span className="font-medium">${item.finalPrice}</span>
+                </h3>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+      <h2 className="mt-10 text-center">Наши двери применяют</h2>
+
+      <Rasm />
+      {/* <Dverxiy /> */}
+      <Input />
+      <Xarita />
+      <hr className="border-2 border-amber-700 mt-8" />
+      <Footer />
+    </div>
+  );
+};
+
+export default KatalogDverey;
+// Sardor
---
+++ src/components/Xarita.jsx
@@ -0,0 +1,113 @@
+const Xarita = () => {
+  return (
+    <>
+      <div className="container mx-auto flex flex-wrap gap-10 mt-12">
+        <div className="flex-1">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2995.701435157919!2d69.24153831542145!3d41.29949587927002!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38aef4f6b8c3b2d1%3A0x8b41bfa5c9c0eafa!2sAmir%20Temur%20Square!5e0!3m2!1sen!2s!4v1691570405772!5m2!1sen!2s"
+            width="100%"
+            height="440"
+            allowFullScreen=""
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+            title="Bizning manzil"
+            className="rounded-lg shadow-md"></iframe>
+        </div>
+        <div className="flex-1">
+          <h2>Где купить?</h2>
+          <div className="mt-7 space-y-4 text-sm h-96 overflow-y-auto scroll-container cursor-pointer">
+            <ul>
+              <li className="items-center">
+                <p className="ms-2">г. Москва, Красная площадь дом 1</p>
+                <p className="ms-2 mb-4">
+                  +7 499 375 77 65 ООО “Рога и копыта”
+                </p>
+              </li>
+
+              <li className="items-center">
+                <p className="ms-2">г. Москва, Красная площадь дом 1</p>
+                <p className="ms-2 mb-4">
+                  +7 499 375 77 65 ООО “Рога и копыта”
+                </p>
+              </li>
+              <li className="items-center">
+                <p className="ms-2">г. Москва, Красная площадь дом 1</p>
+                <p className="ms-2 mb-4">
+                  +7 499 375 77 65 ООО “Рога и копыта”
+                </p>
+              </li>
+              <li className="items-center">
+                <p className="ms-2">г. Москва, Красная площадь дом 1</p>
+                <p className="ms-2 mb-4">
+                  +7 499 375 77 65 ООО “Рога и копыта”
+                </p>
+              </li>
+              <li className="items-center">
+                <p className="ms-2">г. Москва, Красная площадь дом 1</p>
+                <p className="ms-2 mb-4">
+                  +7 499 375 77 65 ООО “Рога и копыта”
+                </p>
+              </li>
+              <li className="items-center">
+                <p className="ms-2">г. Москва, Красная площадь дом 1</p>
+                <p className="ms-2 mb-4">
+                  +7 499 375 77 65 ООО “Рога и копыта”
+                </p>
+              </li>
+              <li className="items-center">
+                <p className="ms-2">г. Москва, Красная площадь дом 1</p>
+                <p className="ms-2 mb-4">
+                  +7 499 375 77 65 ООО “Рога и копыта”
+                </p>
+              </li>
+              <li className="items-center">
+                <p className="ms-2">г. Москва, Красная площадь дом 1</p>
+                <p className="ms-2 mb-4">
+                  +7 499 375 77 65 ООО “Рога и копыта”
+                </p>
+              </li>
+              <li className="items-center">
+                <p className="ms-2">г. Москва, Красная площадь дом 1</p>
+                <p className="ms-2 mb-4">
+                  +7 499 375 77 65 ООО “Рога и копыта”
+                </p>
+              </li>
+              <li className="items-center">
+                <p className="ms-2">г. Москва, Красная площадь дом 1</p>
+                <p className="ms-2 mb-4">
+                  +7 499 375 77 65 ООО “Рога и копыта”
+                </p>
+              </li>
+              <li className="items-center">
+                <p className="ms-2">г. Москва, Красная площадь дом 1</p>
+                <p className="ms-2 mb-4">
+                  +7 499 375 77 65 ООО “Рога и копыта”
+                </p>
+              </li>
+              <li className=" items-center">
+                <p className="ms-2">г. Москва, Красная площадь дом 1</p>
+                <p className="ms-2 mb-4">
+                  +7 499 375 77 65 ООО “Рога и копыта”
+                </p>
+              </li>
+              <li className="items-center">
+                <p className="ms-2">г. Москва, Красная площадь дом 1</p>
+                <p className="ms-2 mb-4">
+                  +7 499 375 77 65 ООО “Рога и копыта”
+                </p>
+              </li>
+              <li className="items-center">
+                <p className="ms-2">г. Москва, Красная площадь дом 1</p>
+                <p className="ms-2 mb-4">
+                  +7 499 375 77 65 ООО “Рога и копыта”
+                </p>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Xarita;
