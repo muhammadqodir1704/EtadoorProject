fc1152560b00ebe2aa146964c88df238c6309e7b
Dilmurod Mamurov <mamurovd426@gmail.com>
2025-02-04 17:39:07 +0500

    Sotrudnichat ozgarishlar

--- src/pages/Sotrudnichat.jsx
+++ src/pages/Sotrudnichat.jsx
@@ -1,13 +1,14 @@
-import React from 'react';
-import Frame1 from '../assets/Frame1.png';
-import Frame2 from '../assets/Frame2.png';
-import Frame3 from '../assets/Frame3.png';
-import Frame4 from '../assets/Frame4.png';
-import Frame5 from '../assets/Frame5.png';
-import Frame6 from '../assets/Frame6.png';
-import Frame7 from '../assets/Frame7.png';
-import Dverxiy from '../components/Dverxiy';
-import { Link } from 'react-router-dom';
+import React from "react";
+import Frame1 from "../assets/Frame1.png";
+import Frame2 from "../assets/Frame2.png";
+import Frame3 from "../assets/Frame3.png";
+import Frame4 from "../assets/Frame4.png";
+import Frame5 from "../assets/Frame5.png";
+import Frame6 from "../assets/Frame6.png";
+import Frame7 from "../assets/Frame7.png";
+import Dverxiy from "../components/Dverxiy";
+import { Link } from "react-router-dom";
+import EtadoorImg from "../components/EtadoorImg";
 
 const Sotrudnichat = () => {
   const items = [
@@ -68,12 +69,10 @@ const Sotrudnichat = () => {
         </div>
         {/* Dverxi img */}
       </div>
-      <EtadoorImg /> <br /> <br /> <br /> <br />
+      <EtadoorImg /> <br /><br /><br /><br />
       <Dverxiy />
     </>
   );
 };
 
-export default Sotrudnichat
-// salom dunyo
-
+export default Sotrudnichat;
