7122cbdc0739b5f8c6d3eaeeae4c9397c7695126
Muhammadqodir <muhammadqodir@Macbook-Pro---valijonov.local>
2025-02-05 16:32:33 +0500

    some

--- src/Modal/ZaprositModal.jsx
+++ src/Modal/ZaprositModal.jsx
@@ -23,7 +23,7 @@ export default function ZaprositModal() {
 
   return (
     <div>
-      {/* <Button onClick={handleOpen}>Open modal</Button> */}
+      <Button onClick={handleOpen}>Open modal</Button>
       <Modal
         open={open}
         onClose={handleClose}
