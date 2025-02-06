5f93779ac85200b763d423b0e093620bba3f2e9e
Muhammadqodir <muhammadqodir@Macbook-Pro---valijonov.local>
2025-02-06 15:44:50 +0500

    Modallar qoshilmoqda

--- src/Modal/ZaprositModal.jsx
+++ src/Modal/ZaprositModal.jsx
@@ -9,21 +9,16 @@ const style = {
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
-  width: 400,
+  width: 540,
+  height: 361,
   bgcolor: 'background.paper',
-  border: '2px solid #000',
   boxShadow: 24,
   p: 4,
 };
 
-export default function ZaprositModal() {
-  const [open, setOpen] = React.useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
-
+export default function ZaprositModal({ open, handleClose }) {
   return (
     <div>
-      <Button onClick={handleOpen}>Open modal</Button>
       <Modal
         open={open}
         onClose={handleClose}
@@ -31,8 +26,8 @@ export default function ZaprositModal() {
         aria-describedby="modal-modal-description"
       >
         <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
+          <Typography className='text-center text-custom-red ' style={{ fontSize: 34, fontWeight: 700 }} id="modal-modal-title" variant="h6">
+            Запросить прайс-лист
           </Typography>
           <Typography id="modal-modal-description" sx={{ mt: 2 }}>
             Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
