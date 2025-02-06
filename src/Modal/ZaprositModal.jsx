162270660b47f90ba2d4b0b9575e646fb3dd0dc6
Muhammadqodir <muhammadqodir@Macbook-Pro---valijonov.local>
2025-02-06 16:47:18 +0500

    1-modal boldi

--- src/Modal/ZaprositModal.jsx
+++ src/Modal/ZaprositModal.jsx
@@ -1,17 +1,17 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import Modal from '@mui/material/Modal';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
 
 const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
   width: 540,
   height: 361,
-  bgcolor: 'background.paper',
+  bgcolor: "#E5E5E5",
   boxShadow: 24,
   p: 4,
 };
@@ -26,12 +26,47 @@ export default function ZaprositModal({ open, handleClose }) {
         aria-describedby="modal-modal-description"
       >
         <Box sx={style}>
-          <Typography className='text-center text-custom-red ' style={{ fontSize: 34, fontWeight: 700 }} id="modal-modal-title" variant="h6">
+          <Typography
+            className="text-center text-custom-red"
+            style={{ fontSize: 34, fontWeight: 700 }}
+            id="modal-modal-title"
+            variant="h6"
+          >
             Запросить прайс-лист
           </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+          <Typography
+            className="text-center text-custom-black"
+            style={{ fontSize: 18, fontWeight: 400 }}
+            id="modal-modal-description"
+          >
+            Оставьте свои контакты <br /> и мы вышлем вам наш прайс-лист{" "}
           </Typography>
+          <div className="mt-3 flex flex-col items-center">
+            <input
+              style={{ width: 450, height: 45 }}
+              type="text"
+              placeholder="Ваше имя"
+              className=" p-2 mt-4 w-full text-center bg-white "
+            />
+            <div className="flex justify-center gap-2">
+              <input
+                style={{ width: 220, height: 45 }}
+                type="text"
+                placeholder="Телефон*"
+                className=" p-2 mt-4 w-full text-center bg-white "
+              />
+              <input
+                style={{ width: 220, height: 45 }}
+                type="text"
+                placeholder="Email*"
+                className=" p-2 mt-4 w-full text-center bg-white "
+              />
+            </div>
+            <div className="flex items-center mt-4 gap-8">
+              <p style={{fontSize:12 , fontWeight:400 , }} className="font-circe">Нажимая на кнопку, вы даете согласие с <br/> Политикой конфеденциальнолсти</p>
+              <button className="bg-custom-red text-white font-circe" style={{width: 182 ,height:39 , fontSize: 14 , fontWeight:700 }}>Отправить заявку</button>
+            </div>
+          </div>
         </Box>
       </Modal>
     </div>
