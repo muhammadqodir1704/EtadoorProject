0fefe34f6e6fb210c9b1c89fd1d08239e5d6b9ba
Muhammadqodir <muhammadqodir@Macbook-Pro---valijonov.local>
2025-02-07 03:00:08 +0500

    good night

--- src/components/Xarita.jsx
+++ src/components/Xarita.jsx
@@ -82,7 +82,7 @@ const Xarita = () => {
   const [selectedCoords, setSelectedCoords] = useState(locations[0].coords);
 
   return (
-    <div className="container mx-auto flex flex-wrap gap-10 mt-12">
+    <div className="container mx-auto flex flex-wrap items-center  gap-10 mt-12">
       <div className="flex-1">
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2995.701435157919!2d69.24153831542145!3d41.29949587927002!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38aef4f6b8c3b2d1%3A0x8b41bfa5c9c0eafa!2sAmir%20Temur%20Square!5e0!3m2!1sen!2s!4v1691570405772!5m2!1sen!2s"
